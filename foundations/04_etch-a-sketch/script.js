fa062e2aca1f6f697d2623243d648cd39aa67b42
brendanharrington <brha8770@colorado.edu>
2025-11-21 18:21:57 -0700

    feat: add functionality to change the grid dimension

--- foundations/04_etch-a-sketch/script.js
+++ foundations/04_etch-a-sketch/script.js
@@ -1,27 +1,48 @@
 const container = document.getElementById('grid-container');
-let squares;
-const value = 16;
+const input = document.querySelector('input');
+const output = document.querySelector('output');
+const resetBtn = document.getElementById('reset-btn');
 
-const updateGrid = () => {
+let squares;
+
+const updateGrid = (value) => {
+  const style = (
+    `height: calc(var(--container-size) / ${value});`
+    + ` width: calc(var(--container-size) / ${value});`  
+  );
+
   container.innerHTML = '';
+  output.textContent = `${input.value}x${input.value}`;
 
   for (let i = 0; i < value; i++) {
-    container.innerHTML += `
-      <div class="grid-row">
-        ${'<div class="grid-square"></div>'.repeat(value)}
-      </div>
-      `;
+    container.innerHTML += (
+      '<div class="grid-row">'
+      + `<div class="grid-square" style="${style}"></div>`.repeat(value)
+      + '</div>'
+    );
   }
 
   squares = document.querySelectorAll('.grid-square');
-};
-
-document.addEventListener('DOMContentLoaded', (event) => {
-  updateGrid(value);
 
   squares.forEach((square) => {
     square.addEventListener('mouseenter', (event) => {
-      square.style.backgroundColor = 'grey';
+      square.style.backgroundColor = 'dimgrey';
     });
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  updateGrid(input.value);
+
+  resetBtn.addEventListener('click', () => {
+    updateGrid(input.value);
+  });
+
+  input.addEventListener('input', (event) => {
+    output.textContent = `${event.target.value}x${event.target.value}`;
+  });
+
+  input.addEventListener('mouseup', (event) => {
+    updateGrid(event.target.value)
   });
 });
