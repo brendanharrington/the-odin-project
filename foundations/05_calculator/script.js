e503ff6e270eb0b020bf082b1b5e94ae4d4d2a3d
brendanharrington <brha8770@colorado.edu>
2025-11-24 16:45:48 -0700

    feat: add basic functionality
    add support for two-number calculations (not much in terms of field validation yet)

--- foundations/05_calculator/script.js
+++ foundations/05_calculator/script.js
@@ -14,7 +14,44 @@ const divide = (num1, num2) => {
   return Number(num1) / Number(num2);
 };
 
-console.log(add('4', '5')); // output: 9
-console.log(subtract('4', '5')); // output: -1
-console.log(multiply('4', '5')); // output: 20
-console.log(divide('4', '5')); // output: 0.8
+document.addEventListener('DOMContentLoaded', () => {
+  const firstNumber = document.querySelector('#first-number');
+  const secondNumber = document.querySelector('#second-number');
+  const operator = document.querySelector('#operator-select')
+  const calculateBtn = document.querySelector('#calculate-btn');
+  const output = document.querySelector('output');
+
+  const resetFields = () => {
+    firstNumber.value = '';
+    secondNumber.value = '';
+  };
+
+  calculateBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    switch (operator.value) {
+      case '+':
+        output.textContent = add(firstNumber.value, secondNumber.value);
+        resetFields();
+        break;
+
+      case '-':
+        output.textContent = subtract(firstNumber.value, secondNumber.value);
+        resetFields();
+        break;
+
+      case '*':
+        output.textContent = multiply(firstNumber.value, secondNumber.value);
+        resetFields();
+        break;
+
+      case '/':
+        output.textContent = divide(firstNumber.value, secondNumber.value);
+        resetFields();
+        break;
+
+      default:
+        break
+    };
+  });
+});
