6f815e399c5ccc894ccb0a4d63f6d2a022636a7a
brendanharrington <brha8770@colorado.edu>
2025-11-25 11:05:47 -0700

    feat: complete calculator app (mostly)
    implement support for two-number calculations

--- foundations/05_calculator/script.js
+++ foundations/05_calculator/script.js
@@ -38,6 +38,56 @@ const operate = (num1, num2, operator) => {
   };
 };
 
+const handleSubmit = (expression) => {
+  const found = expression.match(OPERATORS);
+  let result;
+
+    if (!found) {
+      console.log('returning');
+      return;
+    }
+
+    if (expression[expression.length - 1].match(OPERATORS)) {
+      result = 'SyntaxError';
+      return;
+    }
+
+    const operator = found[0];
+    const [num1, num2] = expression.split(operator);
+    result = operate(num1, num2, operator);
+    return result;
+};
+
+const updateDisplay = (action, prev = null, newChar = null) => {
+  let result;
+
+  switch (action) {
+    case 'ADD':
+      if (prev === 'Cannot divide by zero!') {
+        result = newChar;
+        break;
+      }
+
+      result = prev + newChar;
+      break;
+    case 'DELETE':
+      if (prev === 'Cannot divide by zero!') {
+        result = '';
+        break;
+      }
+
+      result = prev.slice(0, -1);
+      break;
+    case 'CLEAR':
+      break;
+    default:
+      break;
+  }
+  
+
+  return result;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const input = document.querySelector('input');
   const numberBtns = document.querySelectorAll('.number-btn');
@@ -48,13 +98,40 @@ document.addEventListener('DOMContentLoaded', () => {
   const equalsBtn = document.querySelector('.equals-btn');
   const decimalBtn = document.querySelector('.decimal-btn');
 
-  numberBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-      input.value += btn.textContent;
-    });
+  document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '-':
+      case '+':
+      case '*':
+      case '/':
+      case '.':
+        input.value = updateDisplay('ADD', input.value, event.key);
+        break;
+      case 'Backspace':
+        input.value = updateDisplay('DELETE', input.value, event.key);
+        break;
+      case 'c':
+        input.value = '';
+        break;
+      case 'Enter':
+        input.value = handleSubmit(input.value);
+        break;
+      default:
+        break;
+    }
   });
 
-  operatorBtns.forEach(btn => {
+  [...numberBtns, ...operatorBtns, decimalBtn].forEach(btn => {
     btn.addEventListener('click', () => {
       input.value += btn.textContent;
     });
@@ -68,17 +145,24 @@ document.addEventListener('DOMContentLoaded', () => {
     input.value = '';
   });
 
-  decimalBtn.addEventListener('click', () => {
-    input.value += '.';
-  });
-
   equalsBtn.addEventListener('click', (event) => {
     const found = input.value.match(OPERATORS);
 
-    if (found) {
-      const operator = found[0];
-      const [num1, num2] = input.value.split(operator);
-      input.value = operate(num1, num2, operator);
+    if (!found) {
+      console.log('returning');
+      return;
     }
+
+    if (input.value[input.value.length - 1].match(OPERATORS)) {
+      input.value = 'SyntaxError';
+      return;
+    }
+
+    console.log(found)
+
+    const operator = found[0];
+    const [num1, num2] = input.value.split(operator);
+    input.value = operate(num1, num2, operator);
+    
   });
 });
