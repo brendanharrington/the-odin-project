e6dd919a9263d701037ebf46b88f3d5ffc9ce357
brendanharrington <brha8770@colorado.edu>
2025-11-25 20:08:20 -0700

    feat: add form logic/validation

---
+++ fullstack-javascript/01_intermediate-html-and-css/01_sign-up-form/script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const createBtn = document.querySelector('#create-btn');
+  const inputs = document.querySelectorAll('input');
+  const password = document.querySelector('#password');
+  const confirmPassword = document.querySelector('#confirm-password');
+
+  let missingFields = false;
+
+  const resetFields = () => {
+    inputs.forEach(input => {
+      input.value = '';
+    });
+  };
+
+  inputs.forEach(input => {
+    input.addEventListener('change', () => {
+      if (input.value.trim() !== '') {
+        input.style.borderColor = 'black';
+      }
+    })
+  })
+
+  createBtn.addEventListener('click', () => {
+    missingFields = false;
+
+    inputs.forEach(input => {
+      if (input.value.trim() === '') {
+        console.log(input)
+        input.style.borderColor = 'red';
+        missingFields = true;
+      }
+    })
+    
+    if (missingFields) {
+      alert('all fields are required');
+      return;
+    }
+
+    if (password.value !== confirmPassword.value) {
+      password.style.borderColor = 'red';
+      confirmPassword.style.borderColor = 'red';
+      alert('passwords must match');
+      return;
+    }
+
+    const user = {};
+    inputs.forEach(input => {
+      if (input.id !== 'confirm-password') {
+        user[input.id] = input.value
+      }
+    });
+
+    console.log(user)
+
+    resetFields();
+  });
+});
