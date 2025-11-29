d380fbc90940509e6823d21dcc0eab81d2664171
brendanharrington <brha8770@colorado.edu>
2025-11-29 16:35:42 -0700

    feat: complete library app

--- fullstack-javascript/02_javascript/01_library/script.js
+++ fullstack-javascript/02_javascript/01_library/script.js
@@ -1,4 +1,22 @@
-const myLibrary = [];
+const myLibrary = [
+  new Book('A Tale of Two Cities', 'Charles Dickens', 416, false),
+  new Book('The Little Prince', 'Antoine de Saint-Exupéry', 96, false),
+  new Book('The Alchemist', 'Paulo Coelho', 163, false),
+  new Book('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling', 223, false),
+  new Book('And Then There Were None', 'Agatha Christie', 272, false),
+  new Book('Dream of the Red Chamber', 'Cao Xueqin', 329, false),
+  new Book('The Hobbit', 'J.R.R. Tolkien', 310, false),
+  new Book('Alice\'s Adventures in Wonderland', 'Lewis Carroll', 192, false)
+];
+
+const container = document.getElementById('container');
+const tableBody = document.querySelector('tbody');
+const addBtn = document.getElementById('add-btn');
+
+const dialog = document.querySelector('dialog');
+const createBtn = document.getElementById('create-btn')
+const closeBtn = document.getElementById('close-btn');
+const form = document.querySelector('form');
 
 function Book(title, author, pages, read) {
   if (!new.target) {
@@ -16,23 +34,75 @@ function Book(title, author, pages, read) {
   };
 };
 
-const books = [
-  new Book('A Tale of Two Cities', 'Charles Dickens', 416, false),
-  new Book('The Little Prince', 'Antoine de Saint-Exupéry', 96, false),
-  new Book('The Alchemist', 'Paulo Coelho', 163, false),
-  new Book('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling', 223, false),
-  new Book('And Then There Were None', 'Agatha Christie', 272, false),
-  new Book('Dream of the Red Chamber', 'Cao Xueqin', 329, false),
-  new Book('The Hobbit', 'J.R.R. Tolkien', 310, false),
-  new Book('Alice\'s Adventures in Wonderland', 'Lewis Carroll', 192, false)
-];
+const updateDisplay = () => {
+  tableBody.innerHTML = '';
 
-// books.forEach(book => console.log(book.info()))
+  myLibrary.forEach(book => {
+    tableBody.innerHTML += 
+      `<tr>
+        <th scope="row">${book.title}</th>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td>${book.read ? '&#10003;' : '&#10007;'}</td>
+        <td><button class="remove-btn" value="${book.id}">remove</button></td>
+        <td><button class="mark-btn" value="${book.id}">mark as ${book.read ? 'unread' : 'read'}</button></td>
+      </tr>`;
+  });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const container = document.getElementById('container');
+  const removeBtns = document.querySelectorAll('.remove-btn');
+  const markBtns = document.querySelectorAll('.mark-btn');
 
-  books.forEach(book => {
-    container.innerHTML += `<p>${book.info()}</p>`
-  })
-})
+  removeBtns.forEach(button => {
+    button.addEventListener('click', () => {
+      const index = myLibrary.findIndex(book => book.id === button.value);
+      const book = myLibrary[index];
+      
+      if (window.confirm(`Do you want to remove ${book.title} from the list?`)) {
+        myLibrary.splice(index, 1);
+        updateDisplay();
+      };
+    });
+  });
+
+  markBtns.forEach(button => {
+    button.addEventListener('click', () => {
+      const index = myLibrary.findIndex(book => book.id === button.value);
+      myLibrary[index].read = !myLibrary[index].read;
+      updateDisplay();
+    });
+  });
+};
+
+const addBookToLibrary = (newBook) => {
+  const { title, author, pages, read } = newBook;
+  myLibrary.push(new Book(title, author, pages, read));
+  updateDisplay();
+};
+
+updateDisplay();
+
+addBtn.addEventListener('click', () => {
+  dialog.showModal();
+});
+
+closeBtn.addEventListener('click', () => {
+  dialog.close();
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  addBookToLibrary({
+    title: event.target.title.value,
+    author: event.target.author.value,
+    pages: event.target.pages.value,
+    read: event.target.read.value === 'true' ? true : false
+  });
+
+  event.target.title.value = '';
+  event.target.author.value = '';
+  event.target.pages.value = '';
+  event.target.read.value = '';
+
+  dialog.close();
+});
